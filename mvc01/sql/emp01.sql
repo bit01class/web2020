c8b669c60c6b5c80fe043d1dda85957b04818aea
bit01class <bit01class@gmail.com>
2020-01-07 10:21:37 +0900

    mvc2 model

---
+++ mvc01/sql/emp01.sql
@@ -0,0 +1,23 @@
+drop table emp01;
+create table emp01(
+	sabun number primary key,
+	name varchar2(15) not null,
+	nalja date,
+	pay number default 0,
+	etc varchar2(30)
+);
+drop sequence emp01_seq;
+create sequence emp01_seq start with 1000;
+-- dummy
+insert into EMP01 values (emp01_seq.nextval, 'user1',sysdate,1000,'이상무');
+insert into EMP01 values (emp01_seq.nextval, 'user2',sysdate,2000,'이상무');
+insert into EMP01 values (emp01_seq.nextval, 'user3',sysdate,3000,'이상무');
+insert into EMP01 values (emp01_seq.nextval, 'user4',sysdate,4000,'이상무');
+insert into EMP01 values (emp01_seq.nextval, 'user5',sysdate,5000,'이상무');
+commit;
+select *from emp01;
+
+
+
+
+
